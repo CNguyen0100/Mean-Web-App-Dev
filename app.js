b9e85b7394d1825f2f1ba9f508d927c7844a9ba5
Cuong Nguyen <kevinnguyen0100@gmail.com>
2018-02-05 13:21:28 -0500

    Done with module 3 , May have problem because user is local . Need to connect to mongo

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var passport =  require('passport');
+var session = require('express-session')
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var api  = require('./routes/api');
-// var authenticate  = require('./routes/authenticate.js');
+var authenticate = require('./routes/authenticate')(passport);
 
 var app = express();
 
@@ -17,13 +19,23 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(session({
+  secret: 'super duper secret',
+  // resave: false,
+  // saveUninitialized: true,
+  // cookie: { secure: true }
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 app.use('/api', api);
-// app.use('/auth', authenticate);
+app.use('/auth', authenticate);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,6 +43,9 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+//initialize Passport
+var initPassport = require('./passport-init');
+initPassport(passport);
 
 // error handler
 app.use(function(err, req, res, next) {
