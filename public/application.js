75eeb7789521d0ea763337a1ca2b69e50d7f3eb9
Cuong Nguyen <kevinnguyen0100@gmail.com>
2018-03-13 19:37:55 -0400

    done with 3/13

--- public/application.js
+++ public/application.js
@@ -3,7 +3,7 @@
 // console.log("loaded application.js");
 var mainApplicationModuleName = 'mean';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['example']); //inside [] is dependency module
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute','example']); //inside [] is dependency module
 
 angular.element(document).ready(function(){
     angular.bootstrap(document, [mainApplicationModuleName]);
