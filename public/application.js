5155079e4cfca122835e45a5f2d14469cf35fc1f
Cuong Nguyen <kevinnguyen0100@gmail.com>
2018-03-18 00:49:15 -0400

    done with routing

--- public/application.js
+++ public/application.js
@@ -1,10 +1,12 @@
-'use strict';
 
 // console.log("loaded application.js");
 var mainApplicationModuleName = 'mean';
 
 var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute','example']); //inside [] is dependency module
 
+mainApplicationModule.config(['$locationProvider',function($locationProvider){
+  $locationProvider.hashPrefix('!');
+}]);
 angular.element(document).ready(function(){
     angular.bootstrap(document, [mainApplicationModuleName]);
 })
