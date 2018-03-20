fc403851b4802f48f7ee760b64d93abef7f17fdd
Cuong Nguyen <kevinnguyen0100@gmail.com>
2018-03-19 22:49:18 -0400

    add nav bar

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -2,7 +2,7 @@
 exports.render = function(req, res) {
 	// Use the 'response' object to render the 'index' view with a 'title' and 'userFullName' properties
 	res.render('index', {
-		title: 'Hello World',
-		userFullName: req.user ? req.user.fullName : ''
+		title: 'Cuong Mean APP',
+		user: JSON.stringify(req.user)
 	});
 };
--- public/application.js
+++ public/application.js
@@ -2,11 +2,13 @@
 // console.log("loaded application.js");
 var mainApplicationModuleName = 'mean';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute','example']); //inside [] is dependency module
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute','example','users']); //inside [] is dependency module
 
 mainApplicationModule.config(['$locationProvider',function($locationProvider){
   $locationProvider.hashPrefix('!');
 }]);
+
+if (window.location.hash === '#_=_') window.location.hash = '#!';
 angular.element(document).ready(function(){
     angular.bootstrap(document, [mainApplicationModuleName]);
 })
